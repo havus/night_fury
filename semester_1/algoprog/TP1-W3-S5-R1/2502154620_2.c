cadb13c892f95629fbc386d435092a62f4da8300
Hafidz Mahrus <havus.it@gmail.com>
2022-06-26 20:27:34 +0800

    refactor

--- semester_1/algoprog/TP1-W3-S5-R1/2502154620_2.c
+++ semester_1/algoprog/TP1-W3-S5-R1/2502154620_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define basicSalary             2000000
 #define minimumWorkHoursInMonth 176
@@ -24,6 +25,34 @@ int calculatePositionalAllowance(int jobLevel) {
   }
 
   return basicSalary * allowancePercentage;
+}
+
+char* formatPrice(int price) {
+  // TODO: handle 2 decimal
+
+  char* result = malloc(256);
+  char convertedPriceS[256];
+
+  sprintf(convertedPriceS, "%d", price);
+
+  // add prefix rp to the variable result
+  strncat(result, "Rp ", 3);
+
+  for (int i = 0; i < strlen(convertedPriceS); i++) {
+    strncat(result, &convertedPriceS[i], 1);
+
+    int moduloThreeIsZero   = (strlen (convertedPriceS) - i - 1) % 3 == 0;
+    char nextCharIsPresent  = convertedPriceS[i+1] != '\0';
+
+    if (moduloThreeIsZero && nextCharIsPresent) {
+      char titik = '.';
+      strncat(result, &titik, 1);
+    }
+  }
+
+  strncat(result, ",00", 3);
+
+  return result;
 }
 
 double defineAllowancePercentageFromEducation(char golPendidikan[]) {
@@ -74,10 +103,10 @@ int main() {
   int totalSalary = basicSalary + totalPositionalAllowance + totolEducationAllowance + totalOvertimeAllowance;
 
   printf("Nama Karyawan %s\n", employeeName);
-  printf("Tunjangan Jabatan Rp: %d\n", totalPositionalAllowance);
-  printf("Tunjangan Pendidikan Rp: %d\n", totolEducationAllowance);
-  printf("Honor Lembur Rp: %d\n", totalOvertimeAllowance);
-  printf("Total Gaji Rp: %d\n", totalSalary);
+  printf("Tunjangan Jabatan  %s\n", formatPrice(totalPositionalAllowance));
+  printf("Tunjangan Pendidikan Rp %s\n", formatPrice(totolEducationAllowance));
+  printf("Honor Lembur Rp %s\n", formatPrice(totalOvertimeAllowance));
+  printf("Total Gaji Rp %s\n", formatPrice(totalSalary));
 
   // TEST CASE 1:
   // Input:
@@ -89,8 +118,8 @@ int main() {
   // Nama Karyawan John
   // Tunjangan Jabatan Rp 200000 (2.000.000 * 10%)
   // Tunjangan Pendidikan Rp 200000 (2.000.000 * 10%)
-  // Honor Lembur Rp 2442000
-  // Total Gaji Rp 4842000 (2000000 + 200000 + 200000 + 2442000)
+  // Honor Lembur Rp 48000 ((192 - 176) * 3000)
+  // Total Gaji Rp 2.448.000 (2000000 + 200000 + 200000 + 48000)
 
   // TEST CASE 2:
   // Input:
